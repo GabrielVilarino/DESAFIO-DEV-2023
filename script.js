05ef06d8a86f514f8664e928d3d0d896e050b5dc
Gabriel Vilarino <gabriel_vilarino@hotmail.com>
2023-05-04 21:29:37 -0300

    second commit

--- script.js
+++ script.js
@@ -1,18 +1,32 @@
+var btn = document.querySelector('#submitbtn')
+
 function addTimes(){
     var textarea = document.querySelector('#clubes')
 
     var linhas = textarea.value.split("\n")
 
-    var times = {};
+    var times = [];
 
 for(var i = 0; i < linhas.length; i++){
     var linha = linhas[i];
     var partes = linha.split(";");
     var time = partes[0].trim();
     var estado = partes[1].trim();
-    times[time] = estado;
+    times[i] = [time, estado];
 }
 
-var selecionados = document.querySelector('#selecionados')
-selecionados.innerHTML = JSON.stringify(times)
+console.table(times);
+
+    var divTable = document.querySelector("#tabela")
+    var table = document.createElement("table")
+    divTable.appendChild(table)
+    
+    for(var i = 0; i < times.length; i++){
+        var linha = table.insertRow(i);
+        for(var j = 0; j < 2; j++){
+            var coluna = linha.insertCell(j);
+                coluna.innerHTML = times[i][j];
+        
+    }
+}
 }
